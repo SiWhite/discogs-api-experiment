28461425c1a3ed50c8dfc308473a0297931745f7
Simon White <simon.white@proximity.co.nz>
2017-01-13 16:56:26 +1300

    added pagination links

--- js/views/app.js
+++ js/views/app.js
@@ -5,37 +5,58 @@ var headers = {
   'user-agent': 'YourUserAgent/0.0.1 +http://localhost:3000/' // a unique user agent is required
 };
 var discogs = require('../discogs');
+var url = '';
+var pageNumber = 1;
+var searchTerm = '';
 
 var AppView = Backbone.View.extend({
   el: 'body',
 
   events: {
-    "submit": "onSubmit",
+    "submit": "handleSubmit",
+    "click a": "changePage",
   },
 
   initialize: function() {
-
+    //console.log('changePage');
   },
 
-  onSubmit: function(e) {
+  handleSubmit: function(e) {
     e.preventDefault();
     $('#results').html('');
-    var searchTerm = $("#user").val();
-    var template = _.template('<li><%= artist %> - <%= title %></li>');
+    searchTerm = $("#user").val();
+    var releaseTemplate = _.template('<li><%= artist %> - <%= title %></li>');
+    var paginationTemplate = _.template('<li><a href="#" class="page-number" id="<%= number %>"><%= number %></a></li>');
+    url = '/users/'+searchTerm+'/collection?per_page=25&page='+pageNumber.toString();
 
-    discogs(headers, '/users/'+searchTerm+'/collection', function(err, data) {
-      var releases =  data.releases;
-      //var html = [];
+    discogs(headers, url, function(err, data) {
+      var pages = data.pagination.pages;
+      var releases = data.releases;
 
       $.each(releases, function(i, release) {
-        $('#results').append(template({
+        $('#results').append(releaseTemplate({
           artist: release.basic_information.artists[0].name,
           title: release.basic_information.title,
         }));
      });
 
-     //console.log(html);
+     for (i = 1; i < pages; i++) {
+       $('#pages').append(paginationTemplate({
+          number: i
+        }));
+     }
     });
+  },
+
+  changePage: function(e) {
+    e.preventDefault();
+    if (e.currentTarget.id == 'prev') {
+      pageNumber = pageNumber -= 1;
+    } else if (e.currentTarget.id == 'next') {
+      pageNumber = pageNumber += 1;
+    }
+
+    console.log('pageNumber = ', pageNumber);
   }
 });
 
