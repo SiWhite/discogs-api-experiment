db795a90a723858ee62ec48d308027cc26034f0d
Si White <info@silentdesigns.co.nz>
2017-01-13 22:06:40 +1300

    Got pagination working on user collection

--- js/views/app.js
+++ js/views/app.js
@@ -1,8 +1,7 @@
 var Backbone = require('backbone');
-//var Discogs = require('disconnect').Client;
 var headers = {
-  'accept-encoding': 'gzip,deflate',
-  'user-agent': 'YourUserAgent/0.0.1 +http://localhost:3000/' // a unique user agent is required
+	'accept-encoding': 'gzip,deflate',
+	'user-agent': 'YourUserAgent/0.0.1 +http://localhost:3000/' // a unique user agent is required
 };
 var discogs = require('../discogs');
 var url = '';
@@ -10,54 +9,61 @@ var pageNumber = 1;
 var searchTerm = '';
 
 var AppView = Backbone.View.extend({
-  el: 'body',
+	el: 'body',
 
-  events: {
-    "submit": "handleSubmit",
-    "click a": "changePage",
-  },
+	events: {
+		"submit": "handleResults",
+		"click a": "changePage",
+	},
 
-  initialize: function() {
-    //console.log('changePage');
-  },
+	initialize: function() {
+		//console.log('changePage');
+	},
 
-  handleSubmit: function(e) {
-    e.preventDefault();
-    $('#results').html('');
-    searchTerm = $("#user").val();
-    var releaseTemplate = _.template('<li><%= artist %> - <%= title %></li>');
-    var paginationTemplate = _.template('<li><a href="#" class="page-number" id="<%= number %>"><%= number %></a></li>');
-    url = '/users/'+searchTerm+'/collection?per_page=25&page='+pageNumber.toString();
+	handleResults: function(e) {
+		e.preventDefault();
+		$('#results').html('');
+		searchTerm = $("#user").val();
+		var releaseTemplate = _.template('<li><%= artist %> - <%= title %></li>');
+		var paginationTemplate = _.template('<li><a href="#" class="page-number" id="<%= number %>"><%= number %></a></li>');
+		url = '/users/'+searchTerm+'/collection?per_page=25&page='+pageNumber.toString();
 
-    discogs(headers, url, function(err, data) {
-      var pages = data.pagination.pages;
-      var releases = data.releases;
+		discogs(headers, url, function(err, data) {
+			var pages = data.pagination.pages;
+			var releases = data.releases;
 
-      $.each(releases, function(i, release) {
-        $('#results').append(releaseTemplate({
-          artist: release.basic_information.artists[0].name,
-          title: release.basic_information.title,
-        }));
-     });
+			$.each(releases, function(i, release) {
+				$('#results').append(releaseTemplate({
+					artist: release.basic_information.artists[0].name,
+					title: release.basic_information.title
+				}));
+			});
 
-     for (i = 1; i < pages; i++) {
-       $('#pages').append(paginationTemplate({
-          number: i
-        }));
-     }
-    });
-  },
+			if ($('#pages').is(':empty')) {
+				for (i = 0; i < pages; i++) {
+					$('#pages').append(paginationTemplate({
+						number: i+1
+					}));
+				}
+			}
+			$('#pages li a.active').removeClass('active');
+			$('#pages li a#'+pageNumber).addClass('active');
+		});
+	},
 
-  changePage: function(e) {
-    e.preventDefault();
-    if (e.currentTarget.id == 'prev') {
-      pageNumber = pageNumber -= 1;
-    } else if (e.currentTarget.id == 'next') {
-      pageNumber = pageNumber += 1;
-    }
-
-    console.log('pageNumber = ', pageNumber);
-  }
+	changePage: function(e) {
+		e.preventDefault();
+		
+		if (e.currentTarget.id == 'prev') {
+			pageNumber = pageNumber -= 1;
+		} else if (e.currentTarget.id == 'next') {
+			pageNumber = pageNumber += 1;
+		} else if ($(e.currentTarget).hasClass('page-number')) {
+			console.log(e.currentTarget);
+			pageNumber = parseInt(e.currentTarget.id);
+		}
+		this.handleResults(e);
+	}
 });
 
 module.exports = AppView;
--- server.js
+++ server.js
@@ -1,50 +1,11 @@
 var express = require('express');
 var app = express();
-// var Discogs = require('disconnect').Client;
-// var oAuth = new Discogs().oauth();
 
 app.use(express.static('public'));
 
 app.get('*', function(req, res){
   res.sendFile(__dirname);
-
-  // oAuth.getRequestToken(
-  //     'rxPLqJaMTZNbjQCYaXSa',
-  //     'sevVVafyZRVZdkZPxxkzTZBKWhcfQUIF',
-  //     'http://localhost:3000/',
-  //     function(err, requestData){
-  //         // Persist "requestData" here so that the callback handler can
-  //         // access it later after returning from the authorize url
-  //         res.redirect(requestData.authorizeUrl);
-  //         console.log('got here')
-  //     }
-  // );
 });
-
-// app.get('/authorize', function(req, res){
-//     var oAuth = new Discogs().oauth();
-//     oAuth.getRequestToken(
-//       'rxPLqJaMTZNbjQCYaXSa',
-//       'sevVVafyZRVZdkZPxxkzTZBKWhcfQUIF',
-//       'http://localhost:3000/',
-//       function(err, requestData){
-//             // Persist "requestData" here so that the callback handler can
-//             // access it later after returning from the authorize url
-//             res.redirect(requestData.authorizeUrl);
-//       }
-//     );
-// });
-//
-// app.get('/callback', function(req, res){
-//     var oAuth = new Discogs(requestData).oauth();
-//     oAuth.getAccessToken(
-//         req.query.oauth_verifier, // Verification code sent back by Discogs
-//         function(err, accessData){
-//             // Persist "accessData" here for following OAuth calls
-//             res.send('Received access token!');
-//         }
-//     );
-// });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
